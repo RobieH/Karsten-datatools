6afd2a33247d4b015c23470734b9237ec99e83a2
RobieH <robie.hennigar@gmail.com>
2013-08-02 11:55:07 -0300

    general probe code, for 2D or 3D files

--- probeMerge.py
+++ probeMerge.py
@@ -5,6 +5,8 @@ import os
 import re
 import scipy.io as sio
 import multiprocessing
+from datetime import datetime, timedelta
+
 class probe:
     """Creates a probe object, allowing the user to efficiently and
     easily load probe data files.  Takes as input the path to the file.
@@ -18,12 +20,12 @@ class probe:
         self.data_name = ''
 
     def load(self):
-        with open(self.filename, 'r') as f:
-           self.m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
         self._line_count()
         self._data_name()
+        self._get_dim()
         self._array_data()
         self._location()
+        self._depth_array()
 
     def print_file(self):
         with open(self.filename, 'r') as f:
@@ -36,7 +38,7 @@ class probe:
     def _line_count(self):
         lines = 0
         with open(self.filename, 'r') as f:
-           m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)    print indiv_locs
+           m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
 
         readline = m.readline
         while readline():
@@ -47,6 +49,17 @@ class probe:
         index = self.filename.index('_') + 1
         self.data_name = self.filename[index:]
 
+    def _get_dim(self):
+        with open(self.filename, 'r') as f:
+            m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
+        i = 0
+        while i < 18:
+            m.readline()
+            i += 1
+        row = m.readline()
+        self.num_cols = len(row.split())
+        m.close()
+
     def _array_data(self):
         with open(self.filename, 'r') as f:
            m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
@@ -55,12 +68,34 @@ class probe:
             row = m.readline()
             i+= 1
         #initialize arrays
-        data = np.empty((self.size,2))
-        readline = m.readline
+        data = np.empty((self.size,self.num_cols))
         for i in xrange(self.size):
             row = m.readline()
-            data[i,...] = [float(j) for j in row.split()]
-        self.data = data
+            data[i,...] = row.split()
+        self.time = data[:,0]
+        self.data = data[:,1:]
+
+    def _depth_array(self):
+        """creates an array with the probe depths.
+        currently assumes a linear distribution with the
+        first probe on the surface of the ocean."""
+
+        #find the depth of the ocean in the file
+        with open(self.filename, 'r') as f:
+             m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
+        i = 0
+        while i < 14:
+            m.readline()
+            i += 1
+        row = m.readline()
+        ocean_depth = float(row.split()[-1])
+        delta_z = ocean_depth / self.num_cols
+        z = np.empty(self.num_cols)
+
+        for i in xrange(self.num_cols):
+            z[i] = i * delta_z
+        self.z = z
+
 
     def _location(self):
         num = re.search("\d",self.filename).start()
@@ -74,14 +109,16 @@ def load_probe(location_files):
         probe_data = probe(filename)
         probe_data.load()
         mdict[probe_data.data_name] = probe_data.data
+        mdict['time'] = probe_data.time
+        mdict['z'] = probe_data.z
         location = probe_data.location
     saveName = 'Location_' + str(location)
     sio.savemat(saveName, mdict=mdict, oned_as='column')
 
 if __name__ == '__main__':
-    datadir = '/home/robie/Documents/python/probes/'
+    datadir = '/home/robie/Documents/python/Probes/'
     #get list of probe files in the directory.
-    specifer = 'L*'
+    specifer = 'S*'
     files = glob.glob(datadir + specifer)
     #break the files up into the individual locations
     loc = []
@@ -95,10 +132,9 @@ if __name__ == '__main__':
     loc = np.array(loc)
     data_entries = np.unique(loc)
     indiv_locs = []
-    for i in xrange(data_entries):
-        curr_loc = np.where(loc == i + 1)[0]
+    for i in data_entries:
+        curr_loc = np.where(loc == i)[0]
         indiv_locs.append([files[j] for j in curr_loc])
-
     #use multiprocessing to save the probe files
     pool = multiprocessing.Pool()
     pool.map(load_probe, indiv_locs)
