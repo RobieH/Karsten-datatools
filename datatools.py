13b47eafe1d2ccaf6615e8fa82435f4a8fbe73c1
RobieH <robie.hennigar@gmail.com>
2013-08-06 17:19:16 -0300

    datatools, Aug 5

--- datatools.py
+++ datatools.py
@@ -41,9 +41,11 @@ import numexpr as ne
 import glob
 from scipy.io import netcdf
 import scipy.io as sio
+import mmap
+import re
 
 
-def loadnc(datadir, singlename=None, dim='2D'):
+def loadnc(datadir, singlename=None, dim='3D'):
     """Loads a .nc  data file
 
     :Parameters:
@@ -161,28 +163,49 @@ def ncdatasort(data):
 
     return data
 
-def node_finder(XY, neiDir):
-	"""Given an array of long\lat points, find the nearest node
-	to each, and return that node's index.  Reads long/lat from
-	a .nei file
-	"""
-	#read long/lat info from .nei file
-	grid_info = np.genfromtxt(neiDir, skip_header=3)
-	lon_lat = grid_info[:,1:3]
+def node_finder(XY, neiDir, grdDir):
+    """Given an array of long\lat points, find the nearest node
+    to each, and return that node's index.  Reads long/lat from
+    a .nei file
+    """
+    #read long/lat info from .nei file
+    grid_info = np.genfromtxt(neiDir, skip_header=3)
+    #read element data from grd.dat file
+    lon_lat = grid_info[:,1:3]
 
-	ones_matrix = np.ones(lon_lat.T.shape)
-	#create the difference array, for each XY
-	index = []
-	for i in xrange(XY.shape[0]):
-		#compute difference
-		x_diff = lon_lat[:,0] - XY[i,0]
-		y_diff = lon_lat[:,1] - XY[i,1]
-		norm = ne.evaluate("sqrt(x_diff*x_diff+y_diff*y_diff)")
-		index.append(np.argmin(norm))
-	return index
+    with open(grdDir) as f:
+        m = mmap.mmap(f.fileno(),0,prot=mmap.PROT_READ)
+
+    done = False
+    while not done:
+        line = m.readline()
+        if "Cell" in line:
+            numEle = int(line.split()[-1])
+            done = True
+
+    nv_tmp = np.empty((numEle, 5))
+    for i in xrange(numEle):
+        nv_tmp[i,:] = m.readline().split()
+
+    nv = nv_tmp[:,1:4].astype(int)
+    print nv
+    trigrid = mplt.Triangulation(lon_lat[:,0], lon_lat[:,1], nv - 1)
+    trifinder = mplt.TrapezoidMapTriFinder(trigrid)
+
+    elements = []
+    for i in xrange(XY.shape[0]):
+        elements.append(trifinder.__call__(XY[i,0], XY[i,1]).item())
+
+    #find the indices that surround each element
+    index = []
+    for i in elements:
+        index.append(list(nv[i,:]))
+
+    #create the difference array, for each XY
+    return elements, index
 
 
-def tri_finder(XY, data):
+def tri_finder(XY, data, nodes=False):
     """Determines which element a given X, Y value is in.
 
     :Parameters: **XY** -- an N x 2 dimensional array of points to locate in triangles. The first column consists of the x coordinate (long), while the second column consists of the y coordinate (lat).
@@ -213,6 +236,13 @@ def tri_finder(XY, data):
         print "No triangle found for some of the points given.  Please \
             ensure that the following entries in XY are correct: "
         print badInds
+
+    if nodes:
+        #find the nodes that surround an element
+        nodes = []
+        for i in indices:
+            nodes.append(list(data['nv'][i,:]))
+        return indices, nodes
     return indices
 
 
@@ -718,161 +748,159 @@ def nan_index(data, dim='2D'):
 
 
 def merge_nc(datadir, savedir, clean_nans = False, intelligent=False, \
-	dim='2D'):
-	"""Merge data for all .nc files in a particular directory.  Assumes
-	that all the data has the same nele, node (i.e. is for the same grid)
+dim='2D'):
+    """Merge data for all .nc files in a particular directory.  Assumes
+    that all the data has the same nele, node (i.e. is for the same grid)
 
-	:Parameters:
-		**datadir** -- The directory where the .nc files are contained.
+    :Parameters:
+        **datadir** -- The directory where the .nc files are contained.
 
-		**savedir** -- The directory where the output should be saved.
+        **savedir** -- The directory where the output should be saved.
 
-		**clean_nans = {True, False}** -- Optional. If set to True,
-			any time series that contain nans in the data series will be
-			stripped.  Will result in slightly slower code.
+        **clean_nans = {True, False}** -- Optional. If set to True,
+            any time series that contain nans in the data series will be
+            stripped.  Will result in slightly slower code.
 
-		**intelligent = {True, False}** -- Optional.  This is only useful
-			for  data files that have overlap.  If set to True, and two
-			time series overlap, the code will select the time series
-			with the smallest nanFrac.  This will result in a noticable
-			slowdown.  Sets clean_nans to True.
+        **intelligent = {True, False}** -- Optional.  This is only useful
+            for  data files that have overlap.  If set to True, and two
+            time series overlap, the code will select the time series
+            with the smallest nanFrac.  This will result in a noticable
+            slowdown.  Sets clean_nans to True.
 
-		**dim ={'2D', '3D'} ** -- Optional.  The dimension of the datafile,
-			assumed to be 2D unless otherwise specified.
+        **dim ={'2D', '3D'} ** -- Optional.  The dimension of the datafile,
+            assumed to be 2D unless otherwise specified.
 
-	"""
-	#generate a list of all files in the directory
-	files = glob.glob(datadir + '*.nc')
-	numFiles = len(files)
-	#get the proper indices to load the files.
-	ordered_time = time_sorter(datadir)
-	#load the first file
-	singlename = os.path.basename(files[ordered_time[0]])
-	data = loadnc(datadir, singlename=singlename, dim=dim)
-	#name the variables that will be used, according to dimension
-	if dim == '2D':
-		key = ['time', 'ua', 'va', 'zeta']
-	elif dim == '3D':
-		key = ['time', 'ua', 'va', 'zeta', 'u', 'v', 'ww']
-	else:
-		raise Exception('Dim must be 2D or 3D.  Correct this in your \
-			call to nc_merge.')
-	#get the dimensions we will need.
-	nele, node, timeDim = size_check(datadir)
+    """
+    #generate a list of all files in the directory
+    files = glob.glob(datadir + '*.nc')
+    numFiles = len(files)
+    #get the proper indices to load the files.
+    ordered_time = time_sorter(datadir)
+    #load the first file
+    singlename = os.path.basename(files[ordered_time[0]])
+    data = loadnc(datadir, singlename=singlename, dim=dim)
+    #name the variables that will be used, according to dimension
+    if dim == '2D':
+        key = ['time', 'ua', 'va', 'zeta']
+    elif dim == '3D':
+        key = ['time', 'ua', 'va', 'zeta', 'u', 'v', 'ww']
+    else:
+        raise Exception('Dim must be 2D or 3D.  Correct this in your \
+            call to nc_merge.')
+    #get the dimensions we will need.
+    nele, node, timeDim = size_check(datadir)
 
-	#pre-allocate arrays that we will dump the data in, for speed.
-	Time = np.zeros(timeDim)
-	UA = np.zeros((timeDim, nele))
-	VA = np.zeros((timeDim, nele))
-	ZETA = np.zeros((timeDim, node))
-	if dim == '3D':
-		a = data['u'].shape[1]
-		U = np.zeros((timeDim, a, nele))
-		V = np.zeros((timeDim, a, nele))
-		WW = np.zeros((timeDim, a, nele))
-	#The code will now differ depending on whether intelligent was set to
-	#True or not.
-	if not intelligent:
-		#start filling the matrices
-		l = len(data['time'])
-		Time[0:l] = data['time']
-		UA[0:l,:] = data['ua']
-		VA[0:l,:] = data['va']
-		ZETA[0:l,:] = data['zeta']
-		if dim == '3D':
-			U[0:l,...] = data['u']
-			V[0:l,...] = data['v']
-		#placeholder for the last time series
-		nt = l - 1
-		#loop through all files in the directory, remember the first has
-		#already been loaded.
+    #pre-allocate arrays that we will dump the data in, for speed.
+    Time = np.zeros(timeDim)
+    UA = np.zeros((timeDim, nele))
+    VA = np.zeros((timeDim, nele))
+    ZETA = np.zeros((timeDim, node))
+    if dim == '3D':
+        a = data['u'].shape[1]
+        U = np.zeros((timeDim, a, nele))
+        V = np.zeros((timeDim, a, nele))
+        WW = np.zeros((timeDim, a, nele))
+    #The code will now differ depending on whether intelligent was set to
+    #True or not.
+    if not intelligent:
+        #start filling the matrices
+        l = len(data['time'])
+        Time[0:l] = data['time']
+        UA[0:l,:] = data['ua']
+        VA[0:l,:] = data['va']
+        ZETA[0:l,:] = data['zeta']
+        if dim == '3D':
+            U[0:l,...] = data['u']
+            V[0:l,...] = data['v']
+        #placeholder for the last time series
+        nt = l - 1
+        #loop through all files in the directory, remember the first has
+        #already been loaded.
 
-		#first, for a 2D file
-		if dim == '2D':
-			for i in xrange(1, numFiles):
-				#load a file
-				ncid = netcdf.netcdf_file(files[ordered_time[i]],'r')
-				ua_temp = ncid.variables['ua'].data
-				va_temp = ncid.variables['va'].data
-				zeta_temp = ncid.variables['zeta'].data
-				time_temp = ncid.variables['time'].data
-				ncid.close()
+        #first, for a 2D file
+        if dim == '2D':
+            for i in xrange(1, numFiles):
+                #load a file
+                ncid = netcdf.netcdf_file(files[ordered_time[i]],'r')
+                ua_temp = ncid.variables['ua'].data
+                va_temp = ncid.variables['va'].data
+                zeta_temp = ncid.variables['zeta'].data
+                time_temp = ncid.variables['time'].data
+                ncid.close()
 
-				#how many time series could be added, at most
-				ltt = len(time_temp)
+                #how many time series could be added, at most
+                ltt = len(time_temp)
 
-				#discover if there is any overlap in time series
-				start = bisect.bisect_left(Time, time_tmp[0])
+                #discover if there is any overlap in time series
+                start = bisect.bisect_left(Time, time_tmp[0])
 
-				#determine if there was a match.  Note that if there was
-				#no match, bisect returns the last index.
+                #determine if there was a match.  Note that if there was
+                #no match, bisect returns the last index.
 
-				if start != timeDim:
-					#there is a matching index, i.e. the data overlaps
-					NT = ltt + start
-					Time[start_ind:NT] = time_temp
-					UA[start_ind:NT,:] = ua_temp
-					VA[start_ind:NT,:] = va_temp
-					ZETA[start_ind:NT,:] = zeta_temp
-					nt = NT
-				else:
-					#there was no matching index, i.e. the data does not 						overlap
-					numT = ltt + nt
-					Time[nt:numT] = time_temp
-					UA[nt:numT,:] = ua_temp
-					VA[nt:numT,:] = va_temp
-					ZETA[nt:numT,:] = zeta_temp
-					nt += ltt
-			print "Loaded file " + str(i+1) + " of " + str(numFiles) + "."
-		if dim == '3D':
-			for i in xrange(1, numFiles):
-				#load a file
-				ncid = netcdf.netcdf_file(files[ordered_time[i]],'r')
-				ua_temp = ncid.variables['ua'].data
-				va_temp = ncid.variables['va'].data
-				zeta_temp = ncid.variables['zeta'].data
-				time_temp = ncid.variables['time'].data
-				u_temp = ncid.variables['u'].data
-				v_temp = ncid.variables['v'].data
-				ww_temp = ncid.variables['ww'].data
-				ncid.close()
+                if start != timeDim:
+                    #there is a matching index, i.e. the data overlaps
+                    NT = ltt + start
+                    Time[start_ind:NT] = time_temp
+                    UA[start_ind:NT,:] = ua_temp
+                    VA[start_ind:NT,:] = va_temp
+                    ZETA[start_ind:NT,:] = zeta_temp
+                    nt = NT
+                else:
+                    #there was no matching index, i.e. the data does not 						overlap
+                    numT = ltt + nt
+                    Time[nt:numT] = time_temp
+                    UA[nt:numT,:] = ua_temp
+                    VA[nt:numT,:] = va_temp
+                    ZETA[nt:numT,:] = zeta_temp
+                    nt += ltt
+                print "Loaded file " + str(i+1) + " of " + str(numFiles) + "."
+        else:
+            for i in xrange(1, numFiles):
+                #load a file
+                ncid = netcdf.netcdf_file(files[ordered_time[i]],'r')
+                ua_temp = ncid.variables['ua'].data
+                va_temp = ncid.variables['va'].data
+                zeta_temp = ncid.variables['zeta'].data
+                time_temp = ncid.variables['time'].data
+                u_temp = ncid.variables['u'].data
+                v_temp = ncid.variables['v'].data
+                ww_temp = ncid.variables['ww'].data
+                ncid.close()
 
-				#how many time series could be added, at most
-				ltt = len(time_temp)
+                #how many time series could be added, at most
+                ltt = len(time_temp)
 
-				#discover if there is any overlap in time series
-				start = bisect.bisect_left(Time, time_tmp[0])
+                #discover if there is any overlap in time series
+                start = bisect.bisect_left(Time, time_tmp[0])
 
-				#determine if there was a match.  Note that if there was
-				#no match, bisect returns the last index.
+                #determine if there was a match.  Note that if there was
+                #no match, bisect returns the last index.
 
-				if start != timeDim:
-					#there is a matching index, i.e. the data overlaps
-					NT = ltt + start
-					Time[start_ind:NT] = time_temp
-					UA[start_ind:NT,:] = ua_temp
-					VA[start_ind:NT,:] = va_temp
-					ZETA[start_ind:NT,:] = zeta_temp
-					U[start_ind:NT,...] = u_temp
-					V[start_ind:NT,...] = v_temp
-					WW[start_ind:NT,...] = ww_temp
-					nt = NT
-				else:
-					#there was no matching index, i.e. the data does not 						overlap
-					numT = ltt + nt
-					Time[nt:numT] = time_temp
-					UA[nt:numT,:] = ua_temp
-					VA[nt:numT,:] = va_temp
-					ZETA[nt:numT,:] = zeta_temp
-					U[nt:numT,...] = u_temp
-					V[nt:numT,...] = v_Temp
-					WW[nt:numT,...] = ww_temp
-					nt += ltt
-			print "Loaded file " + str(i+1) + " of " + str(numFiles) + "."
-	else:
-		#we will merge the files in a more intelligent way, based on the
-		#fraction of nans, if there is overlap.
-        #put the first time series in the arrays
+                if start != timeDim:
+                    #there is a matching index, i.e. the data overlaps
+                    NT = ltt + start
+                    Time[start_ind:NT] = time_temp
+                    UA[start_ind:NT,:] = ua_temp
+                    VA[start_ind:NT,:] = va_temp
+                    ZETA[start_ind:NT,:] = zeta_temp
+                    U[start_ind:NT,...] = u_temp
+                    V[start_ind:NT,...] = v_temp
+                    WW[start_ind:NT,...] = ww_temp
+                    nt = NT
+                else:
+                    #there was no matching index, i.e. the data does not 						overlap
+                    numT = ltt + nt
+                    Time[nt:numT] = time_temp
+                    UA[nt:numT,:] = ua_temp
+                    VA[nt:numT,:] = va_temp
+                    ZETA[nt:numT,:] = zeta_temp
+                    U[nt:numT,...] = u_temp
+                    V[nt:numT,...] = v_Temp
+                    WW[nt:numT,...] = ww_temp
+                    nt += ltt
+            print "Loaded file " + str(i+1) + " of " + str(numFiles) + "."
+        #now for the intelligent part
+    else:
         l = len(data['time'])
         Time[0:l] = data['time']
         UA[0:l,:] = data['ua']
@@ -906,7 +934,7 @@ def merge_nc(datadir, savedir, clean_nans = False, intelligent=False, \
                 data1 = {}
                 data1['ua'] = UA[start:,...]
                 data1['va'] = VA[start:,...]
-                if dim  == '3D'
+                if dim  == '3D':
                     data1['u'] = U[start:,...]
                     data1['v'] = V[start:,...]
                     data1['ww'] = WW[start:,...]
@@ -943,10 +971,10 @@ def merge_nc(datadir, savedir, clean_nans = False, intelligent=False, \
                 UA[nt:numT,:] = ua_temp
                 VA[nt:numT,:] = va_temp
                 ZETA[nt:numT,:] = zeta_temp
-                    if dim == '3D':
-                        U[nt:numT,...] = u_temp
-                        V[nt:numT,...] = v_temp
-                        WW[nt:numT,...] = ww_temp
+                if dim == '3D':
+                    U[nt:numT,...] = u_temp
+                    V[nt:numT,...] = v_temp
+                    WW[nt:numT,...] = ww_temp
                 nt += ltt
         print "Loaded file " + str(i+1) + " of " + str(numFiles) + "."
     #delete  any extra space at the end of the arrays.
@@ -960,7 +988,8 @@ def merge_nc(datadir, savedir, clean_nans = False, intelligent=False, \
             U = np.delete(U, toDelete, 0)
             V = np.delete(V, toDelete, 0)
             WW = np.delete(WW, toDelete, 0)
-
+    except:
+        pass
     data['time'] = Time
     data['ua'] = UA
     data['va'] = VA
@@ -979,7 +1008,7 @@ def merge_nc(datadir, savedir, clean_nans = False, intelligent=False, \
         if dim == '3D':
             U = np.delete(U, nan_ind, 0)
             V = np.delete(V, nan_ind, 0)
-            WW = np..delete(WW, nan_ind, 0)
+            WW = np.delete(WW, nan_ind, 0)
         data['time'] = Time
         data['ua'] = UA
         data['va'] = VA
